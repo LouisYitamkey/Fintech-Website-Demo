a9d6fda56acc43be6920032d0c2d712908d7b8eb
Kwaku Louis <bonglamgh@gmail.com>
2023-04-17 13:30:37 +0000

    navbar removed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
+
 import Home from "./Pages/Home";
 import { DigitalLending } from "./Pages/DigitalLending";
 import Kyc from "./Pages/Kyc";
@@ -7,14 +7,17 @@ import Kyc from "./Pages/Kyc";
 function App() {
   return (
     <BrowserRouter>
-      <div>
-        <Navbar/>
+ 
         <Routes>
+
           <Route path="/" element={ <Home/> } />
           <Route path="/digitalLending" element={ <DigitalLending/> } />
           <Route path="/kyc" element={ <Kyc/> } />
+        
         </Routes>
-      </div>
+
+      
+    
     </BrowserRouter>
   );
 }
